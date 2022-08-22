6959d939e36966620b08169cb5fc21d55f185e89
romainjocelyn <romainhelard@gmail.com>
2022-08-22 17:09:29 +0200

    AdminPanel

---
+++ src/Controller/Admin/PlaceCrudController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Place;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class PlaceCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Place::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
