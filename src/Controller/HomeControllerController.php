88035a8b792e289387da70782d5eb94d643f526d
romainjocelyn <romainhelard@gmail.com>
2022-08-22 15:36:15 +0200

    Home COOOONTROLLER

--- src/Controller/HomeControllerController.php
+++ src/Controller/HomeControllerController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeCoontrollerController extends AbstractController
+class HomeControllerController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home_coontroller/index.html.twig', [
-            'controller_name' => 'HomeCoontrollerController',
+        return $this->render('home_controller/index.html.twig', [
+            'controller_name' => 'HomeControllerController',
         ]);
     }
 }
