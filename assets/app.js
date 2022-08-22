c9df46ad4a4fe4eb42c7945987745f678f86b3da
romainjocelyn <romainhelard@gmail.com>
2022-08-22 14:04:16 +0200

    Import Bootstrap

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,10 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+
+// You can specify which plugins you need
+import { Tooltip, Toast, Popover } from 'bootstrap';
 
 // start the Stimulus application
 import './bootstrap';
