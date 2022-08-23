74f02ccdf46db6b52fac03a0fc5b8a4e0fa67784
romainjocelyn <romainhelard@gmail.com>
2022-08-23 17:04:14 +0200

    Test Affichage Events

--- src/Controller/HomeControllerController.php
+++ src/Controller/HomeControllerController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\GoOut;
+use App\Repository\GoOutRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeControllerController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(GoOutRepository $gooutrepo): Response
     {
+        // Recherche des evenement en BDD
+        $events = $gooutrepo->findAll();
+
+        // Affichage dans le template : events.nom
         return $this->render('home_controller/index.html.twig', [
             'controller_name' => 'HomeController',
+            'events' => $events
         ]);
     }
 }
