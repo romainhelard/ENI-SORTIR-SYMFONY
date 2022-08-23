c5c96dc8806a35eb5906189f89f4cb8e28b6634e
romainjocelyn <romainhelard@gmail.com>
2022-08-23 15:13:44 +0200

    Langage Admin

--- src/Controller/Admin/CityCrudController.php
+++ src/Controller/Admin/CityCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\City;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class CityCrudController extends AbstractCrudController
@@ -10,6 +11,14 @@ class CityCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return City::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Ville')
+            ->setEntityLabelInPlural('Les Villes')
+        ;
     }
 
     /*
