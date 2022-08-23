30d397488731442663843735763541732cb00857
romainjocelyn <romainhelard@gmail.com>
2022-08-23 10:15:41 +0200

    Dashboard Forms

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,12 +2,18 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use App\Entity\City;
+use App\Entity\GoOut;
+use App\Entity\Place;
+use App\Entity\State;
+use App\Entity\Campus;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -29,12 +35,34 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('ENI SORTIR SYMFONY');
+            ->setTitle('ENI SORTIR.COM');
     }
 
     public function configureMenuItems(): iterable
     {
+        // Création du Dashboard
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        // Création des sections
+        yield MenuItem::section('Evenements');
+        yield MenuItem::linkToCrud('Ajouter un Evenement', 'fas fa-plus', GoOut::class)->setAction(Crud::PAGE_NEW);
+        yield MenuItem::linkToCrud('Voir les Evenements', 'fas fa-eye', GoOut::class);
+
+        yield MenuItem::section('Campus');
+        yield MenuItem::linkToCrud('Ajouter un Campus', 'fas fa-plus', Campus::class)->setAction(Crud::PAGE_NEW);
+        yield MenuItem::linkToCrud('Voir les Campus', 'fas fa-eye', Campus::class);
+
+        yield MenuItem::section('Lieux');
+        yield MenuItem::linkToCrud('Ajouter un Lieu', 'fas fa-plus', Place::class)->setAction(Crud::PAGE_NEW);
+        yield MenuItem::linkToCrud('Voir les Lieux', 'fas fa-eye', Place::class);
+
+        yield MenuItem::section('Ville');
+        yield MenuItem::linkToCrud('Ajouter une Ville', 'fas fa-plus', City::class)->setAction(Crud::PAGE_NEW);
+        yield MenuItem::linkToCrud('Voir les Villes', 'fas fa-eye', City::class);
+
+        yield MenuItem::section('Etat');
+        yield MenuItem::linkToCrud('Ajouter un Etat', 'fas fa-plus', State::class)->setAction(Crud::PAGE_NEW);
+        yield MenuItem::linkToCrud('Voir les Etats', 'fas fa-eye', State::class);
+
+
     }
 }
