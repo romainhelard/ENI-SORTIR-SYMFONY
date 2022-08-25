163e1755c01c42a295256f8e885cfee3ce6b11d6
romainjocelyn <romainhelard@gmail.com>
2022-08-25 14:56:21 +0200

    searchBar JS

--- assets/searchBar.js
+++ assets/searchBar.js
@@ -1,15 +1,16 @@
 function searchBar() {
+
     // Declare variables
-    let input = document.getElementsByClassName('searchBarInput2');
+    let input = document.getElementsByClassName('search_input');
     input = input.value.toLowerCase();
-    let x = document.getElementsByClassName('sortieSearch');
+    let x = document.getElementsByClassName('articleSearch');
 
     // Loop through all list items, and hide those who don't match the search query
     for (let i = 0; i < x.length; i++) {
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
             x[i].style.display="none";
             x[i].style.listStyle="none";
-        }
+        }   
         else {
             x[i].style.display="list-item";
             x[i].style.listStyle="none";
