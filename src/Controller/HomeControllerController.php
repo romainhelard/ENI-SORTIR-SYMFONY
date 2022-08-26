62a0f2b62e35d85d068da503b9e302e59677e99f
CocoA1SportbackSline <corentine.audo@gmail.com>
2022-08-26 16:31:43 +0200

    Affichage du profil utilisateur connecté

--- src/Controller/HomeControllerController.php
+++ src/Controller/HomeControllerController.php
@@ -8,6 +8,7 @@ use App\Entity\Place;
 use Symfony\Component\Mime\Email;
 use App\Repository\GoOutRepository;
 use App\Repository\PlaceRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,12 +17,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeControllerController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(GoOutRepository $gooutrepo, MailerInterface $mailer): Response
+    public function index(GoOutRepository $gooutrepo, UserRepository $users, MailerInterface $mailer): Response
     {
         // Recherche des evenement en BDD
         $events = $gooutrepo->findAll();
-
-        // Affichage dans le template : events.nom
+        
         $email = (new Email())
         ->from('hello@example.com')
         ->to('you@example.com')
@@ -29,10 +29,12 @@ class HomeControllerController extends AbstractController
         ->text('Sending emails is fun again!')
         ->html('<p>See Twig integration for better HTML integration!</p>');
     $mailer->send($email);
-
+        
+    // Affichage dans le template : events.nom
         return $this->render('home_controller/index.html.twig', [
             'controller_name' => 'HomeController',
-            'events' => $events
+            'events' => $events,
+            'users' => $users,
         ]);
     }
 
